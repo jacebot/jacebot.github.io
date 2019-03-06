7036596bda7fdbcbcd32ec8e94fe9f12c7b6ac7d
Jace Whitten <jace.whitten@gmail.com>
2019-03-05 18:45:21 -0800

    Gatsby CV

---
+++ data/siteConfig.js
@@ -0,0 +1,93 @@
+module.exports = {
+  siteTitle: 'Jace Whitten',
+  siteDescription: `Jace Whitten - Frontend Developer`,
+  authorName: 'Jace Whitten',
+  twitterUsername: 'jacewhitten',
+  githubUsername: 'jacebot',
+  authorAvatar: '/images/avatar.jpeg',
+  authorDescription: `Frontend Developer, and passionate about what the projects I am involved with.<br />
+    In 2008 I became interested in web development through necessity. Now 10+ years later I lead teams, build amazing apps and continue to hone my craft.<br/>
+    In 2014, I closed my web development firm to create SaaS model applications for the Cannabis industry. <br />
+    I currently work as a frontend developer and mainly work with <strong>Javascript, React.</strong>`,
+  skills: [
+    {
+      name: 'HTML',
+      level: 90
+    },
+    {
+      name: 'CSS',
+      level: 80
+    },
+    {
+      name: 'Javascript',
+      level: 70
+    },
+    {
+      name: 'React',
+      level: 70
+    },
+    {
+      name: 'Node / Python / Ruby',
+      level: 60
+    },
+    {
+      name: 'Database',
+      level: 50
+    },
+    {
+      name: 'Git',
+      level: 80
+    }
+  ],
+  jobs: [
+    {
+      company: "Lendico",
+      begin: {
+        month: 'apr',
+        year: '2018'
+      },
+      duration: null,
+      occupation: "Frontend developer",
+      description: "I integrate the Frontend team responsible for developing and maintaining the online lending platform."
+  
+    }, {
+      company: "Anapro",
+      begin: {
+        month: 'dec',
+        year: '2016'
+      },
+      duration: '1 yr e 5 mos',
+      occupation: "Fullstack developer",
+      description: "Development and maintenance, corrective and preventive, of web applications for the real estate market."
+    }, {
+      company: "Anapro",
+      begin: {
+        month: 'set',
+        year: '2012'
+      },
+      duration: '4 yrs e 3 mos',
+      occupation: "Support Technician",
+      description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
+  
+    },
+    /* ... */
+  ],
+  social: {
+    linkedin: "https://www.linkedin.com/in/jacewhitten",
+    github: "https://github.com/jacebot",
+  },
+  siteUrl: 'https://jacebot.github.io/cv',
+  pathPrefix: '/cv', // Note: it must *not* have a trailing slash.
+  siteCover: '/images/cover.jpeg',
+  googleAnalyticsId: 'UA-131359385-1',
+  background_color: '#ffffff',
+  theme_color: '#25303B',
+  display: 'minimal-ui',
+  icon: 'src/assets/gatsby-icon.png',
+  headerLinks: [
+    {
+      label: 'Jace Whitten',
+      url: '/',
+    }
+  ]
+}
